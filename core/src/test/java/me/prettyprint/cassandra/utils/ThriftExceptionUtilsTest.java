79fc1b9ac803ee584c9ce5127b2c28e292fa1036
Lee <lee.dickinson@sensis.com.au>
2012-04-27 09:41:03 +1000

    added negative test and minor refactor.

--- core/src/test/java/me/prettyprint/cassandra/utils/ThriftExceptionUtilsTest.java
+++ core/src/test/java/me/prettyprint/cassandra/utils/ThriftExceptionUtilsTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.net.SocketException;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @SuppressWarnings({"ThrowableResultOfMethodCallIgnored"})
@@ -12,20 +13,25 @@ public class ThriftExceptionUtilsTest {
 
   @Test
   public void testBrokenPipeLower() {
-    assertTrue(ThriftExceptionUtils.isBrokenSocket(createException("this is a broken pipe error")));
+    assertTrue(checkException("this is a broken pipe error"));
   }
 
   @Test
   public void testBrokenPipeUpper() {
-    assertTrue(ThriftExceptionUtils.isBrokenSocket(createException(" some BROKEN PIPE problem")));
+    assertTrue(checkException(" some BROKEN PIPE problem"));
   }
 
   @Test
   public void testConnectionResetCamel() {
-    assertTrue(ThriftExceptionUtils.isBrokenSocket(createException("this is a Connection reset error")));
+    assertTrue(checkException("this is a Connection reset error"));
   }
 
-  private TException createException(String message) {
-    return new TException(new SocketException(message));
+  @Test
+  public void testOther() {
+    assertFalse(checkException("some other error"));
+  }
+
+  private boolean checkException(String message) {
+    return ThriftExceptionUtils.isBrokenSocket(new TException(new SocketException(message)));
   }
 }
